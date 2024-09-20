cfabf107dc22d560993cd93e599a5a455404b701
MinhhTien <92145479+MinhhTien@users.noreply.github.com>
2024-09-20 19:19:53 +0700

    feature: update garden with description and images

--- src/garden/dto/update-garden.dto.ts
+++ src/garden/dto/update-garden.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType, PickType } from '@nestjs/swagger'
 import { BaseGardenDto } from './base.garden.dto'
 
-export class UpdateGardenDto extends PartialType(PickType(BaseGardenDto, ['name', 'description', 'gardenManagerId'])) {}
+export class UpdateGardenDto extends PartialType(PickType(BaseGardenDto, ['description', 'images', 'gardenManagerId'])) {}
