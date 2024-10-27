4bd6dce8d2f46188f467118e5ba483f6a58ba9ca
MinhhTien <92145479+MinhhTien@users.noreply.github.com>
2024-10-27 16:50:56 +0700

    feature: view learner my session

--- src/class/services/session.service.ts
+++ src/class/services/session.service.ts
@@ -1,19 +1,24 @@
 import { Injectable, Inject } from '@nestjs/common'
+import * as _ from 'lodash'
 import { IClassRepository } from '@src/class/repositories/class.repository'
 import { Session } from '@class/schemas/session.schema'
 import { Types } from 'mongoose'
+import { ILearnerClassService } from './learner-class.service'
 
 export const ISessionService = Symbol('ISessionService')
 
 export interface ISessionService {
   findOneBy(params: { sessionId: string; classId: string; instructorId?: string }): Promise<Session>
+  findMySession(params: { sessionId: string; classId: string; learnerId: string }): Promise<Session>
 }
 
 @Injectable()
 export class SessionService implements ISessionService {
   constructor(
     @Inject(IClassRepository)
-    private readonly classRepository: IClassRepository
+    private readonly classRepository: IClassRepository,
+    @Inject(ILearnerClassService)
+    private readonly learnerClassService: ILearnerClassService
   ) {}
 
   public async findOneBy(params: { sessionId: string; classId: string; instructorId?: string }) {
@@ -26,6 +31,29 @@ export class SessionService implements ISessionService {
       projection: 'sessions',
       options: { lean: true }
     })
+
+    const sessionIndex = courseClass?.sessions.findIndex((session) => session._id.toString() === sessionId)
+    if (sessionIndex === -1) return null
+
+    return courseClass?.sessions[sessionIndex]
+  }
+
+  public async findMySession(params: { sessionId: string; classId: string; learnerId: string }) {
+    const { sessionId, classId, learnerId } = params
+    const conditions = { _id: classId }
+    
+    const courseClass = await this.classRepository.findOne({
+      conditions,
+      projection: 'sessions',
+      populates: [
+        {
+          path: 'learnerClass',
+          select: ['learnerId']
+        }
+      ],
+      options: { lean: true }
+    })
+    if ((_.get(courseClass, 'learnerClass.learnerId') as string)?.toString() !== learnerId) return null
 
     const sessionIndex = courseClass?.sessions.findIndex((session) => session._id.toString() === sessionId)
     if (sessionIndex === -1) return null
